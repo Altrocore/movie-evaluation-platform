b86a1b7679941988ee83c2e8535970fbce46aa62
Daniel <93489366+Altrocore@users.noreply.github.com>
2023-08-17 11:54:15 -0230

    added json-server, use IMDB json, mapping films in main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,43 @@
-import React from "react";
-import "./main.css"
+import React, { useState, useEffect } from "react";
+import "./main.css";
+import { getFilms } from "../../api";
+import { nanoid } from "nanoid"; 
 
 function Main() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    async function fetchAndSetFilms() {
+      try {
+        const filmsData = await getFilms();
+        const filmsWithIds = filmsData.slice(0, 6).map((film) => ({
+          ...film,
+          id: nanoid(),
+        }));
+        setFilms(filmsWithIds);
+      } catch (error) {
+        console.error("Error fetching films:", error);
+      }
+    }
+
+    fetchAndSetFilms();
+  }, []);
 
   return (
     <main className="main">
-      main component
+      <h1>Films</h1>
+      <div className="film-posters">
+        {films.map((film) => (
+          <img
+            key={film.id}
+            src={film.poster_url}
+            alt={film.title}
+            className="film-poster"
+          />
+        ))}
+      </div>
     </main>
-  )
+  );
 }
 
 export default Main;
