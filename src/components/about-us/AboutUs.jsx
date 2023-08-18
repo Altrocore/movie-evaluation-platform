3171b900117120cdc7b4a60adb24a56a886c1ddf
Alex-Ptpv <oleksii.potapov@keyin.com>
2023-08-18 15:29:35 -0230

    123

--- src/components/about-us/AboutUs.jsx
+++ src/components/about-us/AboutUs.jsx
@@ -3,8 +3,14 @@ import "./about.css";
 function AboutUs() {
 
   return (
-    <div>
-      ABOUT US PAGE
+    <div className="about-us-wrapper">
+      <p className="about-us-description">
+        Welcome to our platform dedicated to the world of films! Our platform is designed to be more than just an extensive collection of movies  – it's a community where you can immerse yourself in the world of films, share your thoughts and follow recommendations.
+      </p>
+      <div className="about-us-info">
+        <h2>Unleash Your Inner Critic</h2>
+        <p>Whether you're a casual moviegoer or an aspiring film critic, this is your space to shine. </p>
+      </div>
     </div>
   )
 }
